d4f8a5014322d53611a6ca56c417053278be2d73
Anusha Raju <anusha.raju0070@gmail.com>
2024-12-05 06:35:24 -0800

    Initial Commit

---
+++ API_MVVM_architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API_MVVM_architecture
+//
+//  Created by Anusha Raju on 12/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ API_MVVM_architectureTests/API_MVVM_architectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  API_MVVM_architectureTests.swift
+//  API_MVVM_architectureTests
+//
+//  Created by Anusha Raju on 12/5/24.
+//
+
+import Testing
+@testable import API_MVVM_architecture
+
+struct API_MVVM_architectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
